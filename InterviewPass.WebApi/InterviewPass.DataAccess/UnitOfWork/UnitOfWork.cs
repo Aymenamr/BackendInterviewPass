8c7da29a2b6da2650b1ab90620072aa9914e547c
Punctuate <58395857+Aymenamr@users.noreply.github.com>
2025-01-16 21:43:16 +0100

    unit

---
+++ InterviewPass.WebApi/InterviewPass.DataAccess/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,22 @@
+﻿using InterviewPass.DataAccess.Entities;
+using InterviewPass.DataAccess.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterviewPass.DataAccess.UnitOfWork
+{
+    public class UnitOfWork
+    {
+        private IGenericRepository<Exam>  _examRepo { get; set; }
+        private IGenericRepository<Question> _questionRepo { get; set; }
+
+        public UnitOfWork()
+        {
+        
+        }
+
+    }
+}
