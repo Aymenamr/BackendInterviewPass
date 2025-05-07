e27075674794b0fe488a54cd217681194f07b2f1
AlaaYassin1 <alaayassin214@gmail.com>
2025-05-07 12:57:33 +0300

    Add Job tables script

---
+++ InterviewPass.WebApi/InterviewPass.DataAccess/job_tables_script.sql
@@ -0,0 +1,48 @@
+CREATE TABLE "Job" (
+	"Id"	TEXT,
+	"Title"	TEXT,
+	"ShortDescription"	TEXT,
+	"ImagePath"	TEXT,
+	"EmploymentTypeId"	TEXT,
+	"experience"	INTEGER,
+	"SkillId"	TEXT,
+	"WorkingSchedule"	TEXT,
+	"Role"	TEXT,
+	"Salary"	REAL,
+	PRIMARY KEY("Id"),
+	FOREIGN KEY("SkillId") REFERENCES "Skill"("Id"),
+	FOREIGN KEY("EmploymentTypeId") REFERENCES "EmploymentType"("Id")
+);
+
+CREATE TABLE "EmploymentType" (
+	"Id"	INTEGER,
+	"Type"	TEXT,
+	PRIMARY KEY("Id")
+);
+CREATE TABLE "Benefits" (
+	"Id"	TEXT,
+	"Name"	TEXT,
+	PRIMARY KEY("Id")
+);
+
+CREATE TABLE "JobBenefit" (
+	"Id"	TEXT,
+	"JobId"	TEXT,
+	"BenefitId"	TEXT,
+	FOREIGN KEY("JobId") REFERENCES "Job"("Id"),
+	FOREIGN KEY("BenefitId") REFERENCES "Benefits"("Id"),
+	PRIMARY KEY("Id")
+);
+
+CREATE TABLE "JobFile" (
+	"Id"	TEXT,
+	"JobId"	TEXT,
+	"FilePath"	TEXT,
+	"FileName"	TEXT,
+	PRIMARY KEY("Id"),
+	FOREIGN KEY("JobId") REFERENCES "Job"("Id")
+);
+
+CREATE INDEX "IX_Job_Title" ON "Job" (
+	"Title"
+);
