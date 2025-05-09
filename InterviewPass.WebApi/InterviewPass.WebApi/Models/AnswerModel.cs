bd37c06cbaccd3fb3ff84a1ad9b38974ddb5a221
shamhoney <shamhoney@SHAMHONEY>
2025-05-09 07:32:53 +0300

    AnswerModel created

---
+++ InterviewPass.WebApi/InterviewPass.WebApi/Models/AnswerModel.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace InterviewPass.WebApi.Models
+{
+    public class AnswerModel
+    {
+        [RegularExpression(@"^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$", ErrorMessage = "ID must be GUID.")]
+        public String? Id { get; set; } = null!;
+		[Required]
+        public String? Name { get; set; }
+        [Required]
+        public String? Type { get; set; }
+        [Required]
+        public String? Content { get; set; }
+        [Required]
+        public double? Score { get; set; }
+        [Required]
+        [Range(1, short.MaxValue, ErrorMessage = "MinScore must be greater than 0.")]
+        public double MinScore { get; set; }
+
+        [Range(1, short.MaxValue, ErrorMessage = "MaxScore must be greater than 0.")]
+        public double? MaxScore { get; set; }
+        public short NbrOfAnswer { get; set; }
+        public List<AnswerModel>? Answers { get; set; }
+    }
+}
