bf5f2f57a8126faff32215bc567eed3404674640
shamhoney <shamhoney@SHAMHONEY>
2025-05-10 02:46:51 +0300

    (QuestionAnswerModel) & (MultipleChoiceQuestionAnswerModel) have been created

---
+++ InterviewPass.WebApi/InterviewPass.WebApi/Models/QuestionAnswer/MultipleChoiceQuestionAnswerModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using InterviewPass.WebApi.Models.Question;
+
+namespace InterviewPass.WebApi.Models.QuestionAnswer
+{
+    public class MultipleChoiceQuestionAnswerModel : QuestionAnswerModel
+    {
+        [Required]
+        public bool AnswerChoice { get; set; }
+        [Required]
+        public PossibilityModel AnswerContent { get; set; }
+    }
+}
---
+++ InterviewPass.WebApi/InterviewPass.WebApi/Models/QuestionAnswer/QuestionAnswerModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using InterviewPass.WebApi.Models.Question;
+using InterviewPass.WebApi.Models;
+
+namespace InterviewPass.WebApi.Models.QuestionAnswer
+{
+    public class QuestionAnswerModel
+    {
+        public string? Id { get; set; } = null!;
+        [Required]
+        public string? Content { get; set; }
+        [Required]
+        public double? Score { get; set; }
+    }
+}
