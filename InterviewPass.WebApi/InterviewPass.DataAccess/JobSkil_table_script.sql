da26a3df1e0ce9ebdedf40a47041fab28abb6e3b
Punctuate <58395857+Aymenamr@users.noreply.github.com>
2025-05-17 13:52:30 +0100

    organize files

--- InterviewPass.WebApi/InterviewPass.DataAccess/JobSkil_table_script.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE "JobSkill" (
-	"Id"	TEXT,
-	"JobId"	TEXT,
-	"SkillId"	TEXT,
-	PRIMARY KEY("Id"),
-	FOREIGN KEY("JobId") REFERENCES "Job"("Id"),
-	FOREIGN KEY("SkillId") REFERENCES "Skill"("Id")
-);
