60f47790c5bd04177bbb5d02e77588d4ad511048
Ibrahim Jamal <ibrahim918648@gmail.com>
2025-05-24 12:04:47 +0300

    Add Result Model

--- InterviewPass.WebApi/InterviewPass.WebApi/Controllers/ResultController.cs
+++ InterviewPass.WebApi/InterviewPass.WebApi/Controllers/ResultController.cs
@@ -8,29 +8,23 @@ namespace InterviewPass.WebApi.Controllers
     [ApiController]
     public class ResultController : ControllerBase
     {
-        // GET: api/<ValuesController>
-        [HttpGet]
-        public IEnumerable<string> Get()
+
+        // POST api/<ValuesController>
+        [HttpPost]
+        public void Create([FromBody] string value)
         {
-            return new string[] { "value1", "value2" };
         }
 
-        // GET api/<ValuesController>/5
+        // GET api/<ValuesController>
         [HttpGet("{id}")]
-        public string Get(int id)
+        public string GetAll(int id)
         {
             return "value";
         }
 
-        // POST api/<ValuesController>
-        [HttpPost]
-        public void Post([FromBody] string value)
-        {
-        }
-
         // PUT api/<ValuesController>/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public void Update(int id, [FromBody] string value)
         {
         }
 
---
+++ InterviewPass.WebApi/InterviewPass.WebApi/Models/ResultModel.cs
@@ -0,0 +1,12 @@
+﻿using InterviewPass.WebApi.Models.Question;
+
+namespace InterviewPass.WebApi.Models
+{
+    public class ResultModel
+    {
+        public int CandidateId { get; set; }
+        public int ExamId { get; set; }
+        public double Score { get; set; }
+        public DateTime SubmittedAt { get; set; }
+    }
+}
