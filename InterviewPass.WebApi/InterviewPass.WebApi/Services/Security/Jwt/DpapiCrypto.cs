8e7dbd0cabfd22a8053d259f8fc3758174b62658
Mohammed Shokor <mohammed.at.shokor@gmail.com>
2026-01-07 08:38:31 +0200

    Static helper for DPAPI-based encryption/decryption.

---
+++ InterviewPass.WebApi/InterviewPass.WebApi/Services/Security/Jwt/DpapiCrypto.cs
@@ -0,0 +1,34 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+public static class DpapiCrypto
+{
+     private static readonly byte[] Entropy =
+        Encoding.UTF8.GetBytes("MySuperSecretEntropyKey@2025!");
+
+    public static string Encrypt(string plainText)
+    {
+        var plainBytes = Encoding.UTF8.GetBytes(plainText);
+
+        var encryptedBytes = ProtectedData.Protect(
+            plainBytes,
+            Entropy,
+            DataProtectionScope.LocalMachine
+        );
+
+        return Convert.ToBase64String(encryptedBytes);
+    }
+
+    public static string Decrypt(string cipherText)
+    {
+        var encryptedBytes = Convert.FromBase64String(cipherText);
+
+        var decryptedBytes = ProtectedData.Unprotect(
+            encryptedBytes,
+            Entropy,
+            DataProtectionScope.LocalMachine
+        );
+
+        return Encoding.UTF8.GetString(decryptedBytes);
+    }
+}
