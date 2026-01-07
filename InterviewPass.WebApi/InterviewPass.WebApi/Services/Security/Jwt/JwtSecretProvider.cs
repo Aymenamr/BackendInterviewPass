69be845d7722ae1a9fd074cd0c1fc0f203a22bd2
Mohammed Shokor <mohammed.at.shokor@gmail.com>
2026-01-07 08:47:10 +0200

    arrange hierarchy

---
+++ InterviewPass.WebApi/InterviewPass.WebApi/Services/Security/Jwt/JwtSecretProvider.cs
@@ -0,0 +1,19 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+public class JwtSecretProvider : IJwtSecretProvider
+{
+    private readonly IConfiguration _configuration;
+
+    public JwtSecretProvider(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    public string GetSecret()
+    {
+        string secret = _configuration["JwtSettings:SecretKey"]
+            ?? throw new InvalidOperationException("JWT Secret not found in configuration.");
+        return DpapiCrypto.Decrypt(secret) ;
+    }
+}
